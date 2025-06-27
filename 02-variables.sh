11c5c8d627173403786c78162a47bdef131da522
SudheerVDandu <iamsudheerdandu@gmail.com>
2025-06-27 13:05:40 +0530

    Done

--- 02-variables.sh
+++ 02-variables.sh
@@ -3,7 +3,7 @@
 $PERSON1:Ramesh
 $PERSON2:Suresh
 
-echo "PERSON1:: Hi PERSON2"
-echo "PERSON2:: Hi PERSON1"
-echo "PERSON1:: I am good PERSON2, how are you?"
-echo "PERSON2:: Iam good PERSON1"
+echo "$PERSON1:: Hi $PERSON2"
+echo "$PERSON2:: Hi $PERSON1"
+echo "$PERSON1:: I am good $PERSON2, how are you?"
+echo "$PERSON2:: Iam good $PERSON1"
