99e43f4853dcd57b7e943fcc2c5371d032812ee7
SudheerVDandu <iamsudheerdandu@gmail.com>
2025-06-27 13:03:49 +0530

    changes done

--- 02-variables.sh
+++ 02-variables.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-echo "Ramesh:: Hi Suresh"
-echo "Suresh:: Hi Ramesh"
-echo "Ramesh:: I am good Suresh, how are you?"
-echo "Suresh:: Iam good Ramesh"
+$PERSON1:Ramesh
+$PERSON2:Suresh
+
+echo "PERSON1:: Hi PERSON2"
+echo "PERSON2:: Hi PERSON1"
+echo "PERSON1:: I am good PERSON2, how are you?"
+echo "PERSON2:: Iam good PERSON1"
